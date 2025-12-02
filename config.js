0c5adcdf3abc5d246b11d254fc1aff878289a5e3
r7000-glitch <rachedumali0807@gmail.com>
2025-12-02 11:33:03 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,4 @@ export const config = {
   SUPABASE_URL: "https://iwwopytnacebtffzcnmq.supabase.co",
   SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d29weXRuYWNlYnRmZnpjbm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTY5OTUsImV4cCI6MjA3OTgzMjk5NX0.kPBBZaw-vfxBkYfYPJOIQFUU3q2vuaIAmfXlAEI-NEM",
 
-  // User accounts
-  users: [
-    { username: "admin", password: "@dmin2026", role: "admin" },
-    { username: "user", password: "user@cf", role: "user" },
-    { username: "viewer", password: "vieweronly", role: "viewer" }
-  ]
 };
