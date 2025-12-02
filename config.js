39783120019068d8fce73409c8e364dd50908fec
r7000-glitch <rachedumali0807@gmail.com>
2025-12-02 10:28:28 -0800

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,9 @@ export const config = {
   SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3d29weXRuYWNlYnRmZnpjbm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNTY5OTUsImV4cCI6MjA3OTgzMjk5NX0.kPBBZaw-vfxBkYfYPJOIQFUU3q2vuaIAmfXlAEI-NEM",
 
   // User accounts
-users: [
-  { username: "admin", password: "@dmin2026", role: "admin" },
-  { username: "user", password: "user@cf", role: "user" },
-  { username: "viewer", password: "vieweronly", role: "viewer" }
-];
-
-}
+  users: [
+    { username: "admin", password: "@dmin2026", role: "admin" },
+    { username: "user", password: "user@cf", role: "user" },
+    { username: "viewer", password: "vieweronly", role: "viewer" }
+  ]
+};
